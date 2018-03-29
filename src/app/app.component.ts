e5f7615913fdd67714bbb0ee468fca8a2f6cff86
djangojazz <brettmorin2@gmail.com>
2018-03-29 07:20:53 -0700

    Got toggle navigation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,10 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   events = [];
   title = 'MoneyEntry';
+  showFiller = false;
 }

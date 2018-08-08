ccdc11cf211e2ed1b39aea4fff558fabef3dd753
djangojazz <brettmorin2@gmail.com>
2018-08-08 08:29:22 -0700

    Category update

--- src/app/Modules/Category/category.module.ts
+++ src/app/Modules/Category/category.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule  } from "@angular/forms";
 import { CommonModule } from '@angular/common';
-import { CategoryComponent } from './category2.component';
+import { CategoryComponent } from './category.component';
 import { MaterialModule } from '../material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { DialogComponent } from '../../Shared/dialog.component';

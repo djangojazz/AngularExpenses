2357a7c832a3ac4508fb637a8a393d424efc122b
djangojazz <brettmorin2@gmail.com>
2019-01-04 04:42:43 -0800

    updated changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseApi: 'http://moneyexpensesapi.azurewebsites.net/expensesApi'
+  baseApi: 'https://moneyexpensesapi.azurewebsites.net/expensesApi'
 };
